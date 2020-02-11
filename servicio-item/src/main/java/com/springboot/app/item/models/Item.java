73a28c96727955bdb2ca729f0d1e8a39d422b204
Jhonatan España <jersi2695@hotmail.com>
2020-02-10 23:52:34 -0500

    servicio de item

---
+++ servicio-item/src/main/java/com/springboot/app/item/models/Item.java
@@ -0,0 +1,37 @@
+package com.springboot.app.item.models;
+
+public class Item {
+	
+	private Producto producto;
+	
+	private int cantidad;
+	
+	private Double total;
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+	
+	
+
+}
