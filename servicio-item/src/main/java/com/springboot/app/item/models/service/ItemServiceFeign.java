e97fdef2af4ef6f3b4bbfe832a8c6af986b653d2
jhoespro <jhonatan.espana@carvajal.com>
2020-03-03 18:32:30 -0500

    Product services

--- servicio-item/src/main/java/com/springboot/app/item/models/service/ItemServiceFeign.java
+++ servicio-item/src/main/java/com/springboot/app/item/models/service/ItemServiceFeign.java
@@ -32,20 +32,17 @@ public class ItemServiceFeign implements IItemService {
 
 	@Override
 	public Producto save(Producto producto) {
-		// TODO Auto-generated method stub
-		return null;
+		return productoClienteRest.crear(producto);
 	}
 
 	@Override
 	public Producto update(Producto producto, Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return productoClienteRest.editar(producto, id);
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-		
+		productoClienteRest.eliminar(id);
 	}
 
 }
